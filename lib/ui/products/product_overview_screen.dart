3916ba3fb82a266885e7015de8f9ddb7b1a10e4d
kimkhanh0101 <khanhb1910529@student.ctu.edu.vn>
2022-09-25 19:37:47 +0700

    Xay sung trang gio hang

--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:myshop/ui/products/cart/cart_manager.dart';
 import 'package:myshop/ui/products/cart/cart_screen.dart';
 
 import 'products_grid.dart';
 
 import './shared/app_drawer.dart';
+
+import 'top_right_badge.dart';
 
 enum FilterOptions { favorites, all }
 
@@ -32,14 +35,17 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
     );
   }
   Widget buildShoppingCartIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
+    return TopRightBadge(
+      data: CartManager().productCount,
+      child: IconButton(
+        icon: const Icon(
+          Icons.shopping_cart,
+        ),
+        onPressed: () {
+          Navigator.of(context).pushNamed(CartScreen.routeName);
+          //print('Go to cart screen');
+        },
       ),
-      onPressed: () {
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-        //print('Go to cart screen');
-      },
     );
   }
   Widget buildProductFilterMenu() {
