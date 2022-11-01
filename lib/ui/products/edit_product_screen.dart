e78ad856cc2c32705e647158b299eea4d56e917e
Your Name <you@example.com>
2022-11-01 13:46:37 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/edit_product_screen.dart
+++ lib/ui/products/edit_product_screen.dart
@@ -64,30 +64,31 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   Future<void> _saveForm() async {
     final isValid = _editForm.currentState!.validate();
-    if(!isValid){
+    if (!isValid) {
       return;
     }
     _editForm.currentState!.save();
     setState(() {
       _isLoading = true;
     });
-    try{
+    try {
       final productsManager = context.read<ProductsManager>();
-      if(_editedProduct.id != null){
-        productsManager.updateProduct(_editedProduct);
-      }else{
-        productsManager.addProduct(_editedProduct);
+      if (_editedProduct.id != null) {
+        await productsManager.updateProduct(_editedProduct);
+      } else {
+        await productsManager.addProduct(_editedProduct);
       }
-    }catch(error){
+    } catch (error) {
       await showErrorDialog(context, 'Something went wrong');
     }
     setState(() {
       _isLoading = false;
     });
-    if(mounted){
+    if (mounted) {
       Navigator.of(context).pop();
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -120,68 +121,71 @@ class _EditProductScreenState extends State<EditProductScreen> {
             ),
     );
   }
-  
+
   TextFormField buildTitleField() {
     return TextFormField(
       initialValue: _editedProduct.title,
       decoration: const InputDecoration(labelText: 'Title'),
       textInputAction: TextInputAction.next,
       autofocus: true,
-      validator: (value){
-        if(value!.isEmpty){
+      validator: (value) {
+        if (value!.isEmpty) {
           return 'Please provide a value.';
         }
         return null;
       },
-      onSaved: (value){
+      onSaved: (value) {
         _editedProduct = _editedProduct.copyWith(title: value);
       },
     );
   }
-  TextFormField buildPriceField(){
+
+  TextFormField buildPriceField() {
     return TextFormField(
       initialValue: _editedProduct.price.toString(),
       decoration: const InputDecoration(labelText: 'Price'),
       textInputAction: TextInputAction.next,
       keyboardType: TextInputType.number,
-      validator: (value){
-        if(value!.isEmpty){
+      validator: (value) {
+        if (value!.isEmpty) {
           return 'Please enter a price.';
         }
-        if(double.tryParse(value) == null){
+        if (double.tryParse(value) == null) {
           return 'Please enter a valid number.';
         }
-        if(double.parse(value) <= 0){
+        if (double.parse(value) <= 0) {
           return 'Please enter a number greater than zero.';
         }
         return null;
       },
-      onSaved: (value){
+      onSaved: (value) {
         _editedProduct = _editedProduct.copyWith(price: double.parse(value!));
       },
     );
   }
-TextFormField buildDescriptionField(){
+
+  TextFormField buildDescriptionField() {
     return TextFormField(
       initialValue: _editedProduct.description,
       decoration: const InputDecoration(labelText: 'Description'),
       maxLines: 3,
       keyboardType: TextInputType.multiline,
-      validator: (value){
-        if(value!.isEmpty){
+      validator: (value) {
+        if (value!.isEmpty) {
           return 'Please enter a description.';
         }
-        if(value.length < 10){
+        if (value.length < 10) {
           return 'Should be at least 10 characters long.';
         }
         return null;
       },
-      onSaved: (value){
+      onSaved: (value) {
         _editedProduct = _editedProduct.copyWith(description: value);
       },
     );
   }
-  Widget buildProductPreview(){
+
+  Widget buildProductPreview() {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.end,
       children: <Widget>[
@@ -192,42 +196,46 @@ TextFormField buildDescriptionField(){
             top: 8,
             right: 10,
           ),
-          decoration: BoxDecoration( 
+          decoration: BoxDecoration(
             border: Border.all(
               width: 1,
               color: Colors.grey,
             ),
           ),
-          child: _imageUrlController.text.isEmpty ? const Text('Enter a URL'):FittedBox( 
-            child: Image.network(
-              _imageUrlController.text,
-              fit: BoxFit.cover,
-            ),
-          ),
+          child: _imageUrlController.text.isEmpty
+              ? const Text('Enter a URL')
+              : FittedBox(
+                  child: Image.network(
+                    _imageUrlController.text,
+                    fit: BoxFit.cover,
+                  ),
+                ),
         ),
-        Expanded(child: buildImageURLField(),
+        Expanded(
+          child: buildImageURLField(),
         ),
       ],
     );
   }
-  TextFormField buildImageURLField(){
+
+  TextFormField buildImageURLField() {
     return TextFormField(
       decoration: const InputDecoration(labelText: 'Image URL'),
       keyboardType: TextInputType.url,
       textInputAction: TextInputAction.done,
       controller: _imageUrlController,
       focusNode: _imageUrlFocusNode,
-      onFieldSubmitted: (value)=>_saveForm(),
-      validator: (value){
-        if(value!.isEmpty){
+      onFieldSubmitted: (value) => _saveForm(),
+      validator: (value) {
+        if (value!.isEmpty) {
           return 'Please enter a image URL.';
         }
-        if(!_isValidImageUrl(value)){
+        if (!_isValidImageUrl(value)) {
           return 'Please enter a valid image URL.';
         }
         return null;
       },
-      onSaved: (value){
+      onSaved: (value) {
         _editedProduct = _editedProduct.copyWith(imageUrl: value);
       },
     );
