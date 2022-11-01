e06d362a60c663554d620fb2fd26318fff7e4b99
Your Name <you@example.com>
2022-11-01 13:30:55 +0700

    Cai dat chung thuc voi Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:myshop/ui/cart/cart_manager.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
 import 'package:myshop/ui/orders/order_manager.dart';
 import 'package:myshop/ui/orders/orders_screen.dart';
 import 'package:myshop/ui/products/edit_product_screen.dart';
 import 'package:myshop/ui/products/user_products_screen.dart';
+import 'package:myshop/ui/splash_screen.dart';
 import 'package:provider/provider.dart';
+import 'ui/auth/auth_screen.dart';
 import 'ui/screens.dart';
+import 'ui/auth/auth_manager.dart';
 
-void main() {
+
+
+void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -18,68 +26,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (ctx) => ProductsManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => CartManager(),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => OrdersManager(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'My Shop',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            fontFamily: 'Lato',
-            colorScheme:
-                ColorScheme.fromSwatch(primarySwatch: Colors.purple).copyWith(
-              secondary: Colors.deepOrange,
-            )),
-        home: const ProductsOverviewScreen(),
-        routes: {
-          CartScreen.routeName: (ctx) => const CartScreen(),
-          OrderScreen.routeName: (ctx) => const OrderScreen(),
-          UserProductsScreen.routeName: (ctx) => const UserProductsScreen(),
-        },
-        onGenerateRoute: (settings) {
-          if (settings.name == ProductDetailScreen.routeName) {
-            final productId = settings.arguments as String;
-            return MaterialPageRoute(
-              builder: (ctx) {
-                return ProductDetailScreen(
-                  ctx.read<ProductsManager>().findById(productId),
-                );
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => AuthManager(),
+          ),
+          // ChangeNotifierProxyProvider<AuthManager, ProductsManager>(
+          //   create: (ctx) => ProductsManager(),
+          //   update: (ctx, authManager, productsManager) {
+          //     productsManager!.authToken = authManager.authToken;
+          //     return productsManager;
+          //   },
+          // ),
+          ChangeNotifierProvider(
+            create: (ctx) => CartManager(),
+          ),
+          ChangeNotifierProvider(
+            create: (ctx) => OrdersManager(),
+          ),
+        ],
+        child: Consumer<AuthManager>(
+          builder: (ctx, authManager, child) {
+            return MaterialApp(
+              title: 'My Shop',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                  fontFamily: 'Lato',
+                  colorScheme:
+                      ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+                          .copyWith(
+                    secondary: Colors.deepOrange,
+                  )),
+              home: authManager.isAuth
+                  ? const ProductsOverviewScreen()
+                  : FutureBuilder(
+                      future: authManager.tryAutoLogin(),
+                      builder: (ctx, snapshot) {
+                        return snapshot.connectionState ==
+                                ConnectionState.waiting
+                            ? const SplashScreen()
+                            : const AuthScreen();
+                      },
+                    ),
+              routes: {
+                CartScreen.routeName: (ctx) => const CartScreen(),
+                OrderScreen.routeName: (ctx) => const OrderScreen(),
+                UserProductsScreen.routeName: (ctx) =>
+                    const UserProductsScreen(),
               },
-            );
-          }
+              onGenerateRoute: (settings) {
+                if (settings.name == ProductDetailScreen.routeName) {
+                  final productId = settings.arguments as String;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return ProductDetailScreen(
+                        ctx.read<ProductsManager>().findById(productId),
+                      );
+                    },
+                  );
+                }
 
-          if (settings.name == EditProductScreen.routeName) {
-            final productId = settings.arguments as String?;
-            return MaterialPageRoute(
-              builder: (ctx) {
-                return EditProductScreen(
-                  productId != null
-                      ? ctx.read<ProductsManager>().findById(productId)
-                      : null,
-                );
+                if (settings.name == EditProductScreen.routeName) {
+                  final productId = settings.arguments as String?;
+                  return MaterialPageRoute(
+                    builder: (ctx) {
+                      return EditProductScreen(
+                        productId != null
+                            ? ctx.read<ProductsManager>().findById(productId)
+                            : null,
+                      );
+                    },
+                  );
+                }
+                return null;
               },
             );
-          }
-          return null;
-        },
-      ),
-    );
+          },
+        ));
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
